2e0a4ee4acaaa1c81d5888b3bcd3ea69bd013e65
Thomas Marks <marksta@umich.edu>
2026-03-03 10:48:03 -0500

    re-export some amisc things

--- src/pem_core/types.py
+++ src/pem_core/types.py
@@ -1,6 +1,10 @@
 from numpy.typing import ArrayLike as ArrayLike, NDArray as NDArray
 from pathlib import Path
+
+# Re-export amisc things
 from amisc import Variable as Variable, Component as Component
+import amisc.distribution as distribution
+import amisc.transform as transform
 
 PathLike = Path | str
 
