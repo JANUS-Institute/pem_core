594a681d50b9699e4c2a0f64cc5379d950271104
Thomas Marks <marksta@umich.edu>
2026-03-03 11:01:16 -0500

    move amisc typing around

--- src/pem_core/types.py
+++ src/pem_core/types.py
@@ -5,9 +5,8 @@ from pathlib import Path
 from amisc import Variable as Variable, Component as Component
 import amisc.distribution as distribution
 import amisc.transform as transform
-import amisc.typing as amisc_types
-import amisc.utils as amisc_utils
+import amisc.typing as amisc
 
 PathLike = Path | str
 
-__all__ = ["Variable", "Component", "NDArray", "ArrayLike", "PathLike", "distribution", "transform", "amisc_types", "amisc_utils"]
+__all__ = ["Variable", "Component", "NDArray", "ArrayLike", "PathLike", "distribution", "transform", "amisc"]
